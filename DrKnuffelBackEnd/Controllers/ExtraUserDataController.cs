4666acffa80281c12d36829bf2ecc8c0813cfcee
YassineChadili <93526956+YassineChadili@users.noreply.github.com>
2026-03-25 11:20:33 +0100

    Add UserDataController

---
+++ DrKnuffelBackEnd/Controllers/ExtraUserDataController.cs
@@ -0,0 +1,46 @@
+﻿using DrKnuffelBackEnd.Repositories;
+using DrKnuffelBackEnd.Repositories.Progress;
+using DrKnuffelBackEnd.Repositories.Step;
+using DrKnuffelBackEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrKnuffelBackEnd.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Consumes("application/json")]
+[Produces("application/json")]
+public class ExtraUserDataController : ControllerBase
+{
+    private readonly IExtraUserData _userData;
+    private readonly IAuthenticationService _authenticationService;
+
+    public ExtraUserDataController(IExtraUserData _userData, IAuthenticationService _authenticationService)
+    {
+        this._userData = _userData;
+        this._authenticationService = _authenticationService;
+    }
+
+    [HttpGet(Name = "GetUserData")]
+    public async Task<ActionResult<List<Models.UserData>>> GetAsync()
+    {
+        string userId = _authenticationService.GetCurrentAuthenticatedUserId();
+        if (!string.IsNullOrEmpty(userId))
+        {
+            var list = await _userData.GetAsyncByUserId(userId);
+            return Ok(list);
+        }
+        return Unauthorized();
+    }
+
+    [HttpPost(Name = "InsertUserData")]
+    public async Task<ActionResult> InsertAsync(Models.UserData userData)
+    {
+        //await _userData.AddAsync(userData);
+        //return Ok();
+
+        userData.Id = Guid.NewGuid();
+        await _userData.AddAsync(userData);
+        return CreatedAtRoute("GetuserDataId", new { objectId = userData.Id }, userData);
+    }
+}
